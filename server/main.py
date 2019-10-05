8d12b3d40c503f5025e2adf7beace14be6bbaebe
Guilherme Guerra <guilhermeguerra12345@gmail.com>
2019-10-05 13:19:23 +0100

    flask criado

--- server/main.py
+++ server/main.py
@@ -1,1 +1,14 @@
-print('hello world')
+import math
+from flask import Flask, jsonify
+
+App = Flask(__name__)
+
+@App.route("/")
+def Index():
+	return jsonify({"about":"good"})
+
+
+
+if __name__ == "__main__":
+	App.run(debug=True)
+
