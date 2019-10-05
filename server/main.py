b21e29cb1fbf868634f1eb265ccb5232146a17d3
Guilherme Guerra <guilhermeguerra12345@gmail.com>
2019-10-05 16:30:34 +0100

    app criado

--- server/main.py
+++ server/main.py
@@ -1,12 +1,20 @@
-import math
-from flask import Flask, jsonify
+import os
+from flask import Flask, request, render_template, url_for, redirect
 
 App = Flask(__name__)
 
-@App.route("/")
+@App.route('/get', methods=['POST', 'GET'])
 def Index():
-	return jsonify({"about":"good"})
 
+	if 'teste' in request.files:
+		teste = request.files['teste']
+		if teste.filename != '':
+			teste.save(os.path.join("C:/Users/guilherme/Desktop/booksSync/booksSync/server/uploads/",teste.filename))
+			return 'ok'
+		else:
+			return 'nop'
+	else:
+		return 'bad request'	
 
 
 if __name__ == "__main__":
